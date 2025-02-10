835f6cb1034fea82fb6e8eba182c0bff4d45c4a8
Tomas Glabazna <tomas@glabazna.cz>
2025-02-10 18:56:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <TButton :label="buttonLabel"/>
+  <div>{{ anotherValue }}</div>
+  <div>{{ fullName }}</div>
+
+</template>
+
+<script setup>
+
+import TButton from '@/components/TButton.vue'
+// export default {
+//   name: 'App',
+//   data() {
+//     return {
+//       buttonLabel: 'click me!'
+//     }
+//   },
+//   components: { TButton }
+// }
+
+import { ref, computed } from 'vue'
+
+const buttonLabel = ref('click me!')
+const anotherValue = ref('lalala')
+anotherValue.value = 'ahoj'
+const firstname = ref('Karel')
+const lastname = ref('Houska')
+
+const fullName = computed(() => firstname.value + ' ' + lastname.value)
+
+
+
+
+</script>
---
+++ src/components/TButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button @click="onClick">{{ label }}</button>
+</template>
+
+<script setup>
+
+// export default {
+//   props: {
+//     label: String
+//   }
+// }
+
+const props = defineProps ({
+  label: String
+})
+
+console.log(props.label)
+
+const onClick = () => {
+  console.log('i have been clicked')
+}
+
+</script>
+
+<style scoped lang="stylus">
+@import '../styles/variables.styl'
+
+button
+  background: $primary-color
+  padding: .35em .7em
+  font-size: 1.2rem
+  border: 1px solid $border-color
+  border-radius: 5px
+  if light($primary-color)
+    color: $dark-text-color
+  else
+    color: $light-text-color
+  cursor: pointer
+  transition: background-color .4s ease
+  &:hover
+    if light($primary-color)
+      background: lighten($primary-color, 30%)
+    else
+      background: darken($primary-color, 30%)
+</style>
